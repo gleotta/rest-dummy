5fc0a192d1def8a982e5b30d77cf7a59ea299e30
jmorando <jmorando@cuyum.com>
2014-04-04 10:52:32 -0300

    Actualización POC

--- src/main/java/com/cuyum/adubo/Bpm6poc.java
+++ src/main/java/com/cuyum/adubo/Bpm6poc.java
@@ -12,6 +12,7 @@ import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.manager.RuntimeEngine;
 import org.kie.api.runtime.process.ProcessInstance;
 import org.kie.api.task.TaskService;
+import org.kie.api.task.model.Status;
 import org.kie.api.task.model.Task;
 import org.kie.api.task.model.TaskSummary;
 import org.kie.services.client.api.RemoteRestRuntimeFactory;
@@ -30,15 +31,31 @@ public class Bpm6poc {
 		} catch (MalformedURLException e) {
 			logger.error("La URL suministrada es incorrecta!",e);
 		}
+		
+		/*--------------------------------------------*/
 		//iniciamos la prueba de concepto del proceso "registro-operadores"
 		Bpm6poc poc = new Bpm6poc(url);
 		poc.connect();
-		//creamos la instancia
-//		ProcessInstance instance = poc.crear_instancia();
-		List<TaskSummary> tareas = poc.obtener_tareas(1l);
-//		poc.verificarSolicitudPorAnalista(tareas.get(0));
-		poc.verificarPresenciaDelSolicitanteEnPlataforma(tareas.get(0));
 		
+		//creamos la instancia
+		ProcessInstance instance = poc.crear_instancia();
+		System.out.println("Estado de la instancia del proceso: "+instance.getState());
+		List<TaskSummary> tareas = new ArrayList<TaskSummary>(); 
+		tareas = poc.obtener_tareas(instance.getId());
+		
+		poc.verificarSolicitudPorAnalista(tareas.get(0));
+		tareas = poc.obtener_tareas(instance.getId());
+		
+		poc.destrabarEsperarAlSolicitante(tareas.get(0));
+		tareas = poc.obtener_tareas(instance.getId());
+		
+		poc.verificarSolicitudPorPlataforma(tareas.get(0));
+		tareas = poc.obtener_tareas(instance.getId());
+		
+		poc.completarRegistro(tareas.get(0));
+		/*--------------------------------------------*/
+		
+		poc.obtener_instancias_activas();
 	}
 	
 	/* POC */
@@ -47,7 +64,7 @@ public class Bpm6poc {
     private String definitionId = "bo.gob.aduana.registro-operadores";
     private URL deploymentUrl;
 
-    private String userId = "viviana";
+    private String userId = "viviana"; //usuario con roles: "tecAnalista", "tecPlataforma" y "Solicitante"  ;
     private String password = "cuyum123.";
     
     private RuntimeEngine engine;
@@ -62,16 +79,39 @@ public class Bpm6poc {
     	if(deploymentUrl!=null){
     		
     		// Creamos la sesion REST
+    		//creamos factoria con timeout
     		RemoteConfiguration configuration = new RemoteConfiguration(deploymentId, deploymentUrl, userId, password,30);
     		engine = new RemoteRuntimeEngine(configuration);
+    		
+    		//creamos factoria sin timeout
 //    		RemoteRestRuntimeFactory restSessionFactory = new RemoteRestRuntimeFactory(deploymentId, deploymentUrl, userId, password);
 //    		engine = restSessionFactory.newRuntimeEngine();
+    		
     		session = engine.getKieSession();
     	}
     }
 
+    public List<ProcessInstance> obtener_instancias_activas(){
+    	List<ProcessInstance> instances = new ArrayList<ProcessInstance>();
+    	instances.addAll(session.getProcessInstances());
+    	
+    	if(instances.isEmpty()){
+    		System.out.println("No ha instancias activas de procesos.");
+    		return instances;
+    	}
+    	for (ProcessInstance processInstance : instances) {
+    		System.out.println("================================");
+    		System.out.println("Proceso: "+processInstance.getProcessId());
+    		System.out.println("Instancia: "+processInstance.getId());
+    		System.out.println("Estado:"+processInstance.getState());
+    		System.out.println("================================");
+		}
+    	return instances;
+    }
+    
     public ProcessInstance crear_instancia() {
 		//Creamos una bolsa de parametros a enviar para iniciar la instancia
+    	//estos parametros son necesarios para que el proceso corra
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("agenciaDesp", "false");
 		params.put("idSolicitud", "tres-3");
@@ -105,7 +145,7 @@ public class Bpm6poc {
     public void verificarSolicitudPorAnalista(TaskSummary taskSummary){
 		//se trata de obtener la tarea
 		Task task = taskService.getTaskById(taskSummary.getId());
-		System.out.println("Ubico la task " + task.getId() + ": " + taskSummary.getName() +"("+taskSummary.getStatus());
+		System.out.println("Ubico la task " + task.getId() + ": " + taskSummary.getName() +" ("+taskSummary.getStatus()+")");
 		
 		//establecer que la tarea esta en progreso
 		taskService.start(task.getId(), userId);
@@ -122,7 +162,7 @@ public class Bpm6poc {
 		
     }
     
-    public void verificarPresenciaDelSolicitanteEnPlataforma(TaskSummary taskSummary){
+    public void destrabarEsperarAlSolicitante(TaskSummary taskSummary){
 		//se trata de obtener la tarea
 		Task task = taskService.getTaskById(taskSummary.getId());
 		System.out.println("Ubico la task " + task.getId() + ": " + taskSummary.getName() +" ("+taskSummary.getStatus()+")");
@@ -142,17 +182,56 @@ public class Bpm6poc {
 		
     }
     
+    public void verificarSolicitudPorPlataforma(TaskSummary taskSummary){
+		//se trata de obtener la tarea
+		Task task = taskService.getTaskById(taskSummary.getId());
+		System.out.println("Ubico la task " + task.getId() + ": " + taskSummary.getName() +" ("+taskSummary.getStatus()+")");
+		
+		//establecer que la tarea esta en progreso
+		taskService.start(task.getId(), userId);
+		
+		//creo una bolsa de variables que permitiran el completado correcto de la tarea
+		Map<String, Object> taskParams = new HashMap<String, Object>();
+		taskParams.put("observacionOut", "false");
+		taskParams.put("editaFormOut", "false"); //el proceso terminará por "Completar Registro", no es necesario
+		
+		//reclamo la tarea
+//		taskService.claim(task.getId(), userId);
+		
+		//completo la tarea
+		taskService.complete(task.getId(), userId, taskParams);
+		
+    }
+    
+    public void completarRegistro(TaskSummary taskSummary){
+		//se trata de obtener la tarea
+		Task task = taskService.getTaskById(taskSummary.getId());
+		System.out.println("Ubico la task " + task.getId() + ": " + taskSummary.getName() +" ("+taskSummary.getStatus()+")");
+		
+		//establecer que la tarea esta en progreso
+		taskService.start(task.getId(), userId);
+		
+		//creo una bolsa de variables que permitiran el completado correcto de la tarea
+		Map<String, Object> taskParams = new HashMap<String, Object>();
+		
+		//reclamo la tarea
+//		taskService.claim(task.getId(), userId);
+		
+		//completo la tarea
+		taskService.complete(task.getId(), userId, taskParams);
+		
+    }
+    
     
     /* HELPER METHOD */
     private List<TaskSummary> findTaskId(long procInstId, List<TaskSummary> taskSumList) {
         List<TaskSummary> taskList = new ArrayList<TaskSummary>();
         for (TaskSummary task : taskSumList) {
+        	System.out.println("Se encuentra el task "+task.getName()+" (" + task.getId() +") para el proceso "+procInstId);
             if (task.getProcessInstanceId() == procInstId) {
             	taskList.add(task); 
-                System.out.println("Se encuentra el task "+task.getName()+" (" + task.getId() +") para el proceso "+procInstId);
             }
         }
-        
         return taskList;
     }
 
