3520179203b0c7ddb06fa02ee3567de1340ae16b
jmorando <jmorando@cuyum.com>
2014-04-10 11:39:05 -0300

    Se eliminar ejemplo BPM6 REST ya que se ha creado un repositorio específico para esto. https://github.com/cuyum/adubo

--- src/main/java/com/cuyum/adubo/util/CustomRequestScopedProducer.java
+++ src/main/java/com/cuyum/adubo/util/CustomRequestScopedProducer.java
@@ -1,47 +1,48 @@
-/*
- * JBoss, Home of Professional Open Source
- * Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual
- * contributors by the @authors tag. See the copyright.txt in the
- * distribution for a full listing of individual contributors.
+/**
+ * Copyright 2014 JBoss Inc
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.cuyum.adubo.util;
 
+
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
 
 import org.apache.log4j.Logger;
 
-/**
- * This class uses CDI to alias Java EE resources, such as the persistence context, to CDI beans
- * 
- * <p>
- * Example injection on a managed bean field:
- * </p>
- * 
- * <pre>
- * &#064;Inject
- * private EntityManager em;
- * </pre>
- */
-public class Resources {
-    // use @SuppressWarnings to tell IDE to ignore warnings about field not being referenced directly
-//    @Produces
-//    @PersistenceContext
-//    @Default
-//    private EntityManager em;
+@RequestScoped
+public class CustomRequestScopedProducer {
 
+//	private static Logger log = Logger.getLogger(CustomRequestScopedProducer.class);
+
+    
+    @Produces
+    @PersistenceContext
+    public EntityManager em; 
+
+    public void close(@Disposes EntityManager em) {
+        em.close();
+    }
+    
     @Produces
     public Logger produceLog(InjectionPoint injectionPoint) {
         return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
